212d74019eb26b4ed2dcd121d4509ce79cbab77e
IvaneHo <10608@holbertonstudents.com>
2025-04-15 14:39:07 +0200

    mise a jour des prototypes et debut de main.c

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
 #include "shell.h"
+
+/**  
+* main - Point d'entrée de notre shell (./hsh)
+* @ac: nombre d'arguments (non utilisé ici)
+* @av: tableau des arguments (non utilisé ici)
+* @env: tableau des variables d'environnement (utilisé pour execve et env)
+*
+*Return: 0 à la fin du programme
+*/
+int main(int ac, char **av, char **env)
+{
+	char *line = NULL;     /* ligne tapée par l'utilisateur */
+	char **argv = NULL;    /* tableau de mots (commande + arguments) */
+
+	(void)ac; /* On ignore ac/av car on ne les utilise pas dans ce shell */
+	(void)av;
+
+	
+}
--- shell.h
+++ shell.h
@@ -23,7 +23,28 @@ extern char **environ;
 
 /* Prototypes ici */
 
-/* main.c */
-int main(int ac, char **av, char **env);
+/* -------- main.c -------- */
+int main(int ac, char **av, char **env);   /* Fonction principale */
+void prompt_display(void);                 /* Affiche le prompt "$ " */
 
-#endif /* SHELL_H */
+/* -------- input.c -------- */
+char *read_input(void);                    /* Lit une ligne avec getline */
+char *trim_newline(char *line);            /* Supprime le \\n final */
+int is_line_empty(char *line);             /* Vérifie si la ligne est vide ou blanche */
+
+/* -------- parser.c -------- */
+char **parse_line(char *line);             /* Découpe la ligne avec strtok en argv */
+void free_tokens(char **tokens);           /* Libère argv[] */
+
+/* -------- builtins.c -------- */
+int is_builtin(char *cmd);                 /* Vérifie si c’est un builtin (exit/env) */
+int handle_builtin(char **argv, char **env); /* Exécute les built-ins */
+
+/* -------- executor.c -------- */
+int execute_command(char **argv, char **env); /* fork + execve + wait */
+
+/* -------- path_utils.c -------- */
+char *find_command_path(char *cmd, char **env); /* Cherche le chemin d’une commande dans $PATH */
+int is_executable(char *full_path);             /* Vérifie si un fichier est exécutable */
+
+#endif /* SHELL_H */ /* Fin de la protection */
